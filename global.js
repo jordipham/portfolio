48269ae98cb96afaac660933325b5e6d55841802
jordipham <jop037@ucsd.edu>
2025-04-16 14:34:18 -0700

    Completed steps 2.1-2.2 - found the navlinks array and found current page

--- global.js
+++ global.js
@@ -4,3 +4,11 @@ console.log("IT'S ALIVE!");
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2.1 - NavLinks
+const navLinks = $$("nav a");
+console.log(navLinks);//verification
+
+// Step 2.2 - Finding the current page
+const currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+console.log(currentLink);//verification
