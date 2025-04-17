4ee6cc386c8b48f69c0205def1f2d15f501bf491
jordipham <jop037@ucsd.edu>
2025-04-16 17:25:02 -0700

    Full completion of Step 2

--- global.js
+++ global.js
@@ -5,10 +5,14 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// Step 2.1 - NavLinks
+// Step 2 Section
+// Step 2.1 - array of NavLinks
 const navLinks = $$("nav a");
 console.log(navLinks);//verification
 
-// Step 2.2 - Finding the current page
+// Step 2.2 - Finding link to the current page
 const currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
 console.log(currentLink);//verification
+
+// Step 2.3 - add current class to current page link
+currentLink?.classList.add('current');
