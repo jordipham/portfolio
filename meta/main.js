54828ee467f6bc18231a5b98d226c2920295d4af
jordipham <jop037@ucsd.edu>
2025-05-09 12:29:25 -0700

    Updated main js for meta page with commits parser and csv data reader

--- meta/main.js
+++ meta/main.js
@@ -1,17 +1,54 @@
 // imports
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
+// data reader
 async function loadData() {
-    const data = await d3.csv('loc.csv', (row) => ({
-      ...row,
-      line: Number(row.line), // or just +row.line
-      depth: Number(row.depth),
-      length: Number(row.length),
-      date: new Date(row.date + 'T00:00' + row.timezone),
-      datetime: new Date(row.datetime),
-    }));
-    // console.log(data);
-    return data;
-  }
-  
-  let data = await loadData();
+  const data = await d3.csv("loc.csv", (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + "T00:00" + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+  // console.log(data);
+  return data;
+}
+
+// modifying the commits we read in
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: "https://github.com/vis-society/lab-7/commit/" + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, "lines", {
+        value: lines,
+        configurable: false,
+        writable: false,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+}
+
+// data from csv
+let data = await loadData();
+//   console.log(data)
+
+// commits viewer
+let commits = processCommits(data);
+//   console.log(commits)
